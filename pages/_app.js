de57f06339b104ccfd4b1f1bb9d08bc33552c934
Jason Brown <browniefed@gmail.com>
2020-12-12 11:30:56 -0800

    Update to latest

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import { Fragment } from "react";
 import "../styles/base.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Fragment>
       <Head>
         <title>Tailwindcss Emotion</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </Fragment>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,38 @@
-import tw from "@tailwindcssinjs/macro";
+import xw from "xwind/macro";
 import styled from "@emotion/styled";
 import { css, cx } from "@emotion/css";
 
-const Button = styled.button(tw`
+const Button = styled.button(xw`
   bg-indigo-600
   hover:bg-indigo-500
-  focus[outline-none border-indigo-700 shadow-outline-indigo]
+  focus[outline-none border-indigo-700 ring]
   active:bg-indigo-700
   transition duration-150 ease-in-out
 `);
 
-const base = tw`relative flex justify-center w-64 min-w-full px-4 py-2 text-sm font-medium leading-5 text-white border border-transparent rounded-md `;
+const base = xw`relative flex justify-center w-64 min-w-full px-4 py-2 text-sm font-medium leading-5 text-white border border-transparent rounded-md `;
 const styles = {
   base: base,
-  button: tw`
+  button: xw`
     bg-teal-600
     hover:bg-teal-500
-    focus[outline-none border-teal-700 shadow-outline-teal]
+    focus[outline-none border-teal-700 ring ]
     active:bg-teal-700
     transition duration-150 ease-in-out
   `,
 
   cssBase: css(base),
-  cssButton: css(tw`
+  cssButton: css(xw`
     bg-gray-600
     hover:bg-gray-500
-    focus[outline-none border-gray-700 shadow-outline-gray]
+    focus[outline-none border-gray-700 ring]
     active:bg-gray-700
     transition duration-150 ease-in-out
   `),
 };
 
 const Index = () => (
-  <div css={tw`grid items-center justify-center h-screen`}>
+  <div css={xw`grid items-center justify-center h-screen`}>
     <Button css={styles.base}>Emotion + Tailwind</Button>
     <button css={[styles.base, styles.button]}>Emotion + Tailwind</button>
     <button className={cx(styles.cssBase, styles.cssButton)}>
